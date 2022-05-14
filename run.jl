eda98fdebc02208ec41374a18c04985358df7d69
Yutai Zhou <yutai.zhou@ll.mit.edu>
2022-05-14 15:21:41 -0400

    subplot achieved

--- run.jl
+++ run.jl
@@ -10,60 +10,95 @@ outputdir(args...) = projectdir("output", args...)
 outputdir_subpath = outputdir(time_now)
 mkdir(outputdir_subpath)
 
-model = Model(Ipopt.Optimizer)
-set_optimizer_attribute(model, "max_cpu_time", 60.0)
-set_optimizer_attribute(model, "print_level", 0)
-
-
-@variable(model, Z[1:31] ≥ 0)
-
+# Matrices for differencing and adding two consecutive elements of a vector
 D = zeros(30,31)
 D[diagind(D)] .= -1
 D[diagind(D,1)] .= 1
-f = sum((D * Z).^2) * 30 / sqrt(1.6^2 -1)
-@objective(model, Min, f)
 
 A = zeros(30,31)
 A[diagind(D)] .= 1
 A[diagind(D,1)] .= 1
 
-# @constraints(model, begin
-#     Z[1]  == 0
-#     Z[31] == 0 
-# end)
+function add_constraint_case1(model)
+    Z = model[:Z]
+    @constraints(model, 
+    begin
+        Z[1]  == 0
+        Z[31] == 0 
+    end
+    )
+end
 
-# @constraints(model, begin
-#     Z[1]  == 0
-#     Z[31] == 0 
-#     Z[16] ≥  0.21
-# end)
+function add_constraint_case2(model)
+    Z = model[:Z]
+    @constraints(model, 
+    begin
+        Z[1]  == 0
+        Z[31] == 0
+        Z[16] ≥  0.21
+    end
+    )
+end
 
-# @constraints(model, begin
-#     Z[1]  == 0
-#     Z[31] == 0
-#     sum(A * Z) == 15/2
-# end)
+function add_constraint_case3(model)
+    Z = model[:Z]
+    @constraints(model, 
+        begin
+            Z[1]  == 0
+            Z[31] == 0
+            sum(A * Z) == 15/2
+        end
+    )
+end
 
-@constraints(model, begin
-    Z[1]  == 0
-    Z[31] == 0
-    Z[16] ≥  0.21
-    sum(A * Z) == 15/2
-end)
+function add_constraint_case4(model)
+    Z = model[:Z]
+    @constraints(model, 
+    begin
+        Z[1]  == 0
+        Z[31] == 0
+        Z[16] ≥  0.21
+        sum(A * Z) == 15/2
+    end
+    )
+end
 
-
-print(model)
-optimize!(model)
-println(solution_summary(model))
-# @show termination_status(model)
-# @show primal_status(model)
-# @show dual_status(model)
-# @show objective_value(model)
-# @show value.(Z)
-
+constraint_fns = [add_constraint_case1, add_constraint_case2, add_constraint_case3, add_constraint_case4]
+constraint_names = ["Closed Body", "Closed Body + Min Thickness", "Closed Body + Fixed Area", "All Constraints"]
 x = range(-1,1,length=31)
-output = value.(Z)
+plots = []
+for (c_fn, c_name) in zip(constraint_fns, constraint_names)
+    model = Model(Ipopt.Optimizer)
+    set_optimizer_attribute(model, "max_cpu_time", 60.0)
+    set_optimizer_attribute(model, "print_level", 0)
 
-plot([x, x],[output,-output], xlabel="x", ylabel="z", legend=false)
+    @variable(model, Z[1:31] ≥ 0)
+    @objective(model, Min,
+        sum((D * Z).^2) * 30 / sqrt(1.6^2 -1)
+    )
 
-savefig(outputdir(outputdir_subpath, "foil.png"))
+    c_fn(model)
+    optimize!(model)
+
+    obj_value = objective_value(model)
+    output = value.(Z)
+
+    push!(plots,
+        plot([x, x],[output,-output],
+            xlabel="x", ylabel="z", legend=false,
+            ylims=[-0.2, 0.2],
+            title=c_name, titlefontsize=10
+        )
+    )
+
+    if c_name == "Closed Body"
+        plot([x, x],[output,-output],
+            xlabel="x", ylabel="z", legend=false,
+            title=c_name, titlefontsize=10
+        )
+        savefig(outputdir(outputdir_subpath, "case1.png"))
+    end
+end
+
+plot(plots...)
+savefig(outputdir(outputdir_subpath, "all.png"))
